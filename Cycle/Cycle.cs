ba86e30555de9db37eae0aa9d306d9fb85855eec
Aleksandr <74274875+Exploit89@users.noreply.github.com>
2022-04-26 22:14:26 +1000

    PC_Home commit Part2 IJunior Cycles

---
+++ Cycle/Cycle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Cycle
+{
+    internal class Cycle
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ваш текст:");
+            string userText = Console.ReadLine();
+            Console.WriteLine("Введите число повторений текста");
+            int userRepeatCount = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; userRepeatCount > 0; i++)
+            {
+                Console.WriteLine(userText);
+                userRepeatCount--;
+            }
+        }
+    }
+}
---
+++ CycleExit/CycleExit.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CycleExit
+{
+    internal class CycleExit
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст:");
+            string userText = Console.ReadLine();
+
+            while (userText != "exit")
+            {
+                string result = userText + userText;
+                Console.WriteLine(result);
+                Console.WriteLine("Для выхода из программы введите \"exit\"");
+                Console.WriteLine("Введите новый текст:");
+                userText = Console.ReadLine();
+            }
+        }
+    }
+}
