33c6bc7f1e986b3a345b15604c2a11550428abc3
Exploit89 <khudajbergenov89@gmail.com>
2022-04-27 16:43:11 +1000

    PC_Work commit

--- CurrencyConverter/CurrencyConverter.cs
+++ CurrencyConverter/CurrencyConverter.cs
@@ -6,45 +6,45 @@ namespace CurrencyConverter
     {
         static void Main(string[] args)
         {
-            int rurToUsd = 70;
-            int rurToJpy = 20;
-            int usdToRur = 65;
-            int usdToJpy = 15;
-            int jpyToUsd = 17;
-            int jpyToRur = 22;
-
-            float usd;
-            float rur;
-            float jpy;
+            float rubToUsd = 70f;
+            float rubToJpy = 20f;
+            float usdToRub = 0.65f;
+            float usdToJpy = 0.15f;
+            float jpyToUsd = 17f;
+            float jpyToRub = 0.22f;
             float currencyCount;
+            string userInput = "0";
 
-            while (true)
+            Console.Write("Введите баланс долларов: ");
+            float usd = Convert.ToSingle(Console.ReadLine());
+            Console.Write("Введите баланс рублей: ");
+            float rub = Convert.ToSingle(Console.ReadLine());
+            Console.Write("Введите баланс йен: ");
+            float jpy = Convert.ToSingle(Console.ReadLine());
+
+            while (userInput != "7")
             {
                 Console.WriteLine("Выберите опцию для обмена:" +
                 "\n1 - Обмен рублей на доллары" +
-                "\n2 - покупка йен за рубли" +
-                "\n3 - покупка рублей за доллары" +
-                "\n4 - покупка йен за доллары" +
-                "\n5 - покупка долларов за йены" +
-                "\n6 - покупка рублей за йены" +
-                "\n0 - завершить операцию");
-                string userInput = Console.ReadLine();
+                "\n2 - Обмен рублей на йены" +
+                "\n3 - Обмен долларов на рубли" +
+                "\n4 - Обмен долларов на йены" +
+                "\n5 - Обмен йен на доллары" +
+                "\n6 - Обмен йен на рубли" +
+                "\n7 - завершить операцию");
+                userInput = Console.ReadLine();
 
                 switch (userInput)
                 {
                     case "1":
                         Console.WriteLine("Обмен рублей на доллары.");
-                        Console.Write("Введите баланс рублей: ");
-                        rur = Convert.ToSingle(Console.ReadLine());
-                        Console.Write("Введите баланс долларов: ");
-                        usd = Convert.ToSingle(Console.ReadLine());
                         Console.WriteLine("Сколько рублей вы хотите обменять?");
                         currencyCount = Convert.ToSingle(Console.ReadLine());
-                        if (rur >= currencyCount)
+                        if (rub >= currencyCount)
                         {
-                            rur -= currencyCount;
-                            usd += currencyCount / rurToUsd;
-                            Console.WriteLine($"Ваш баланс: {rur} рублей и {usd} долларов.\n");
+                            rub -= currencyCount;
+                            usd += currencyCount / rubToUsd;
+                            Console.WriteLine($"Ваш баланс: {rub} рублей, {usd} долларов, {jpy} йен.\n");
                         }
                         else
                         {
@@ -53,17 +53,13 @@ namespace CurrencyConverter
                         break;
                     case "2":
                         Console.WriteLine("Обмен рублей на йены.");
-                        Console.Write("Введите баланс рублей: ");
-                        rur = Convert.ToSingle(Console.ReadLine());
-                        Console.Write("Введите баланс йен: ");
-                        jpy = Convert.ToSingle(Console.ReadLine());
                         Console.WriteLine("Сколько рублей вы хотите обменять?");
                         currencyCount = Convert.ToSingle(Console.ReadLine());
-                        if (rur >= currencyCount)
+                        if (rub >= currencyCount)
                         {
-                            rur -= currencyCount;
-                            jpy += currencyCount / rurToJpy;
-                            Console.WriteLine($"Ваш баланс: {rur} рублей и {jpy} йен.\n");
+                            rub -= currencyCount;
+                            jpy += currencyCount / rubToJpy;
+                            Console.WriteLine($"Ваш баланс: {rub} рублей, {usd} долларов, {jpy} йен.\n");
                         }
                         else
                         {
@@ -72,17 +68,13 @@ namespace CurrencyConverter
                         break;
                     case "3":
                         Console.WriteLine("Обмен долларов на рубли.");
-                        Console.Write("Введите баланс долларов: ");
-                        usd = Convert.ToSingle(Console.ReadLine());
-                        Console.Write("Введите баланс рублей: ");
-                        rur = Convert.ToSingle(Console.ReadLine());
-                        Console.WriteLine("Сколько рублей вы хотите обменять?");
+                        Console.WriteLine("Сколько долларов вы хотите обменять?");
                         currencyCount = Convert.ToSingle(Console.ReadLine());
                         if (usd >= currencyCount)
                         {
                             usd -= currencyCount;
-                            rur += currencyCount / usdToRur;
-                            Console.WriteLine($"Ваш баланс: {usd} долларов и {rur} рублей.\n");
+                            rub += currencyCount / usdToRub;
+                            Console.WriteLine($"Ваш баланс: {rub} рублей, {usd} долларов, {jpy} йен.\n");
                         }
                         else
                         {
@@ -91,17 +83,13 @@ namespace CurrencyConverter
                         break;
                     case "4":
                         Console.WriteLine("Обмен долларов на йены.");
-                        Console.Write("Введите баланс долларов: ");
-                        usd = Convert.ToSingle(Console.ReadLine());
-                        Console.Write("Введите баланс йен: ");
-                        jpy = Convert.ToSingle(Console.ReadLine());
                         Console.WriteLine("Сколько долларов вы хотите обменять?");
                         currencyCount = Convert.ToSingle(Console.ReadLine());
                         if (usd >= currencyCount)
                         {
                             usd -= currencyCount;
                             jpy += currencyCount / usdToJpy;
-                            Console.WriteLine($"Ваш баланс: {usd} долларов и {jpy} йен.\n");
+                            Console.WriteLine($"Ваш баланс: {rub} рублей, {usd} долларов, {jpy} йен.\n");
                         }
                         else
                         {
@@ -110,17 +98,13 @@ namespace CurrencyConverter
                         break;
                     case "5":
                         Console.WriteLine("Обмен йен на доллары.");
-                        Console.Write("Введите баланс йен: ");
-                        jpy = Convert.ToSingle(Console.ReadLine());
-                        Console.Write("Введите баланс долларов: ");
-                        usd = Convert.ToSingle(Console.ReadLine());
                         Console.WriteLine("Сколько йен вы хотите обменять?");
                         currencyCount = Convert.ToSingle(Console.ReadLine());
                         if (jpy >= currencyCount)
                         {
                             jpy -= currencyCount;
                             usd += currencyCount / jpyToUsd;
-                            Console.WriteLine($"Ваш баланс: {jpy} йен и {usd} долларов.\n");
+                            Console.WriteLine($"Ваш баланс: {rub} рублей, {usd} долларов, {jpy} йен.\n");
                         }
                         else
                         {
@@ -129,25 +113,22 @@ namespace CurrencyConverter
                         break;
                     case "6":
                         Console.WriteLine("Обмен йен на рубли.");
-                        Console.Write("Введите баланс йен: ");
-                        jpy = Convert.ToSingle(Console.ReadLine());
-                        Console.Write("Введите баланс рублей: ");
-                        rur = Convert.ToSingle(Console.ReadLine());
                         Console.WriteLine("Сколько йен вы хотите обменять?");
                         currencyCount = Convert.ToSingle(Console.ReadLine());
                         if (jpy >= currencyCount)
                         {
                             jpy -= currencyCount;
-                            rur += currencyCount / jpyToRur;
-                            Console.WriteLine($"Ваш баланс: {jpy} йен и {rur} рублей.\n");
+                            rub += currencyCount / jpyToRub;
+                            Console.WriteLine($"Ваш баланс: {rub} рублей, {usd} долларов, {jpy} йен.\n");
                         }
                         else
                         {
                             Console.WriteLine("Недопустимое кол-во йен\n");
                         }
                         break;
-                    case "0":
-                        return;
+                    case "7":
+                        userInput = "7";
+                        break;
                 }
             }
         }
