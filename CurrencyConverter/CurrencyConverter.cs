6577ca03b14fbff7eacd4d8eab26bc3d9ed029e9
Exploit89 <khudajbergenov89@gmail.com>
2022-04-27 14:20:16 +1000

    PC_Work commit

---
+++ CurrencyConverter/CurrencyConverter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyConverter
+{
+    internal class CurrencyConverter
+    {
+        static void Main(string[] args)
+        {
+            int rurToUsd = 70;
+            int rurToJpy = 20;
+            int usdToRur = 65;
+            int usdToJpy = 15;
+            int jpyToUsd = 17;
+            int jpyToRur = 22;
+
+            float usd;
+            float rur;
+            float jpy;
+            float currencyCount;
+
+            while (userInput != "0")
+            {
+                Console.WriteLine("Выберите опцию для обмена:" +
+                "\n1 - Обмен рублей на доллары" +
+                "\n2 - покупка йен за рубли" +
+                "\n3 - покупка рублей за доллары" +
+                "\n4 - покупка йен за доллары" +
+                "\n5 - покупка долларов за йены" +
+                "\n6 - покупка рублей за йены" +
+                "\n0 - завершить операцию");
+                string userInput = Console.ReadLine();
+
+
+                switch (userInput)
+                {
+                    case "1":
+                        Console.WriteLine("Обмен рублей на доллары.");
+                        Console.Write("Введите баланс рублей: ");
+                        rur = Convert.ToSingle(Console.ReadLine());
+                        Console.Write("Введите баланс долларов: ");
+                        usd = Convert.ToSingle(Console.ReadLine());
+                        Console.WriteLine("Сколько рублей вы хотите обменять?");
+                        currencyCount = Convert.ToSingle(Console.ReadLine());
+                        if (rur >= currencyCount)
+                        {
+                            rur -= currencyCount;
+                            usd += currencyCount / rurToUsd;
+                            Console.WriteLine($"Ваш баланс: {rur} рублей и {usd} долларов.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Недопустимое кол-во рублей");
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
--- RandomSum/RandomSum.cs
+++ RandomSum/RandomSum.cs
@@ -6,19 +6,22 @@ namespace RandomSum
     {
         static void Main(string[] args)
         {
+            int firstDivider = 3;
+            int secondDivider = 5;
+            int maxNumber = 101;
             Random random = new Random();
-            int randomNumber = random.Next(0, 101);
+            int randomNumber = random.Next(0, maxNumber);
             Console.WriteLine("Выбранное число: " + randomNumber);
             int result = 0;
 
             for (int i = 0; i <= randomNumber; i++)
             {
-                if (i % 3 == 0 || i % 5 == 0)
+                if (i % firstDivider == 0 || i % secondDivider == 0)
                 {
                     result += i;
                 }
             }
-            Console.WriteLine("Сумма чисел до выбранного включительно, кратных 3 и 5: " + result);
+            Console.WriteLine($"Сумма чисел до выбранного включительно, кратных {firstDivider} и {secondDivider}: {result}");
         }
     }
 }
