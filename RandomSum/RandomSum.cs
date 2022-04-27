894a47bba4e49a498a5a57d3f85a3fe10d33997e
Exploit89 <khudajbergenov89@gmail.com>
2022-04-27 10:38:54 +1000

    PC_Work commit

---
+++ RandomSum/RandomSum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomSum
+{
+    internal class RandomSum
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(0, 101);
+        }
+    }
+}
---
+++ Sequence/Sequence.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Sequence
+{
+    internal class Sequence
+    {
+        static void Main(string[] args)
+        {
+            int incrementNumber = 7;
+            int startNumber = 5;
+            int endNumber = 100;
+
+            for (int i = startNumber; i < endNumber; i += incrementNumber)
+                Console.Write(i + " ");
+        }
+    }
+}
