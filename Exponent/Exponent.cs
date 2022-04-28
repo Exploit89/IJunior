6ce2a305576d69348378d5b449031e49faf47d34
Exploit89 <khudajbergenov89@gmail.com>
2022-04-28 12:58:55 +1000

    PC_Work commit

---
+++ Exponent/Exponent.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exponent
+{
+    internal class Exponent
+    {
+        static void Main(string[] args)
+        {
+            int minRandomNumber = 10;
+            int maxRandomNumber = 500;
+            int exponentCount = 1;
+            int exponentResult = 1;
+            int exponintiableNumber = 2;
+
+            Random randomNumber = new Random();
+            int givenNumber = randomNumber.Next(minRandomNumber, maxRandomNumber);
+
+            for(int i = exponintiableNumber; i <= givenNumber; i*=exponintiableNumber)
+            {
+                exponentCount++;
+            }
+
+            for(int i = 0; i < exponentCount; i++)
+            {
+                exponentResult *= exponintiableNumber;
+            }
+
+            Console.WriteLine("Выбранное число: " + givenNumber);
+            Console.WriteLine("Степень числа: " + exponentCount);
+            Console.WriteLine($"Двойка в степени {exponentCount} равна {exponentResult}");
+            Console.WriteLine($"{givenNumber} < {exponentResult}");
+        }
+    }
+}
