ed12342c7d1bf0929751c49b05417682bb0fc516
Aleksandr <74274875+Exploit89@users.noreply.github.com>
2022-05-01 21:44:59 +1000

    PC_Home commit Chapter 3 homework

---
+++ Array/Array.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Array
+{
+    internal class Array
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[,] array = new int[4, 5];
+            int minRandomNumber = 1;
+            int maxRandomNumber = 10;
+            int rowsIndex = 0;
+            int columnsIndex = 1;
+            int secondRowIndex = 1;
+            int secondRowSum = 0;
+            int firstColumnIndex = 0;
+            int firstColumnProduct = 1;
+            Console.WriteLine("Состав массива:");
+
+            for (int i = 0; i < array.GetLength(rowsIndex); i++)
+            {
+                for(int j = 0; j < array.GetLength(columnsIndex); j++)
+                {
+                    array[i, j] = random.Next(minRandomNumber, maxRandomNumber);
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            for(int i = 0; i < array.GetLength(secondRowIndex); i++)
+            {
+                secondRowSum += array[secondRowIndex, i];
+            }
+            Console.WriteLine("\nСумма второй строки: " + secondRowSum);
+
+            for(int i = 0; i < array.GetLength(firstColumnIndex); i++)
+            {
+                firstColumnProduct *= array[i, firstColumnIndex];
+            }
+            Console.WriteLine($"\nПроизведение первого столбца: { firstColumnProduct} \n");
+        }
+    }
+}
---
+++ BiggestElement/BiggestElement.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace BiggestElement
+{
+    internal class BiggestElement
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[,] array = new int[10, 10];
+            int minRandomNumber = 1;
+            int maxRandomNumber = 50;
+            int rowsIndex = 0;
+            int columnsIndex = 1;
+            int biggestNumber = 0;
+            int numberReplaceBiggestNumbers = 0;
+            Console.WriteLine("Исходный массив:");
+
+            for (int i = 0; i < array.GetLength(rowsIndex); i++)
+            {
+                for (int j = 0; j < array.GetLength(columnsIndex); j++)
+                {
+                    array[i, j] = random.Next(minRandomNumber, maxRandomNumber);
+
+                    if (array[i, j] > biggestNumber)
+                        biggestNumber = array[i, j];
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine($"\nНаибольшее число в массиве: " + biggestNumber);
+            Console.WriteLine("\nНовый массив:");
+
+            for (int i = 0; i < array.GetLength(rowsIndex); i++)
+            {
+                for (int j = 0; j < array.GetLength(columnsIndex); j++)
+                {
+                    if (array[i, j] == biggestNumber)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        array[i, j] = numberReplaceBiggestNumbers;
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+
+                    if(array[i, j] == numberReplaceBiggestNumbers)
+                        Console.ForegroundColor = ConsoleColor.Red;
+                    else
+                        Console.ForegroundColor = ConsoleColor.White;
+
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
