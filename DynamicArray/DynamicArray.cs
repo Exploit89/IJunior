4f3b9eded7d54936181ab9b5822e51575fd24bca
Exploit89 <khudajbergenov89@gmail.com>
2022-05-04 17:46:36 +1000

    PC_Work commit

--- DynamicArray/DynamicArray.cs
+++ DynamicArray/DynamicArray.cs
@@ -10,6 +10,7 @@ namespace DynamicArray
             int[] incrementArray = new int[array.Length + 1];
             int lastIndexDecrement = 1;
             int sum = 0;
+            int userInputNumber;
 
             Console.WriteLine("Команда для суммирования введеных чисел - sum\nКоманда для выхода - exit");
             Console.WriteLine("Введите число для сохранения в массив или команду: ");
@@ -27,17 +28,34 @@ namespace DynamicArray
                         Console.WriteLine("Сумма значений массива = " + sum);
                         break;
                     default:
+                        userInputNumber = Convert.ToInt32(userInput);
+
                         for (int i = 0; i < array.Length; i++)
                         {
-                            incrementArray[i] = array[i];
-                            incrementArray[incrementArray.Length - lastIndexDecrement] = Convert.ToInt32(userInput);
-                            array = incrementArray;
-                            sum += array[i];
-                        }
-                        incrementArray = new int[array.Length + 1];
+                            if (i != 0)
+                            {
+                                incrementArray[i] = array[i];
+                                sum += array[i];
+                            }
+                            else
+                            {
+                                array[i] = userInputNumber;
+                                incrementArray[i] = array[i];
+                                sum += array[i];
+                            }
+                }
+                        //incrementArray[incrementArray.Length - lastIndexDecrement] = userInputNumber;
+                        array = incrementArray;
                         break;
                 }
             }
         }
     }
 }
+
+/*1. Массивы изначально должны быть пустыми. 
+ * 2. incrementArray[incrementArray.Length - lastIndexDecrement] = Convert.ToInt32(userInput); - зачем это в цикле? 
+ * 3. В целом достаточно своеобразно реализовано расширение массива, посмотрите еще раз пример в лекциях. 
+ * 4. сумма будет выводиться неправильно. Вы тестировали программу перед тем как отправить? 
+ * Не надо, пожалуйста, отправлять заведомо нерабочий код, если есть вопросы, то на платформе есть чат, где Вы можете их задать.
+*/
