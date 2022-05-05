acdf0d7d477349574b92bccab1f2e66bcd7a2dfb
Aleksandr <74274875+Exploit89@users.noreply.github.com>
2022-05-05 21:46:29 +1000

    PC_Home commit

---
+++ ArrayShift/ArrayShift.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ArrayShift
+{
+    internal class ArrayShift
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 8, 6, 7, 5, 3, 0, 9 };
+            int firstNumber;
+
+            Console.WriteLine("Исходный массив: ");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Введите число, на которое необходимо сдвинуть значения массива влево");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            for(int i=0; i < userInput; i++)
+            {
+                firstNumber = array[0];
+
+                for(int j = 0; j < array.Length - 1; j++)
+                {
+                    array[j] = array[j + 1];
+                }
+
+                array[array.Length - 1] = firstNumber;
+            }
+
+            Console.WriteLine("Новый массив: ");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Split/Split.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Split
+{
+    internal class Split
+    {
+        static void Main(string[] args)
+        {
+            string someString = "Тестовая строка с текстом, которую надо бы распилить по словам.\n";
+            Console.WriteLine(someString);
+            string[] dividedString = someString.Split(' ');
+
+            foreach(string word in dividedString)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
