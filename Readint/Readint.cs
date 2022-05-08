b2d2caf4873aef5f63e57a6859ce0202710fb427
Aleksandr <74274875+Exploit89@users.noreply.github.com>
2022-05-08 15:27:33 +1000

    PC_Home commit

---
+++ Readint/Readint.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Readint
+{
+    internal class Readint
+    {
+        static void Main(string[] args)
+        {
+            bool successParse = false;
+
+            while (successParse == false)
+            {
+                Console.WriteLine("Введите число для конвертации в тип int");
+                string userInput = Console.ReadLine();
+                ParseToInt(userInput, ref successParse);
+            }
+        }
+
+        static void ParseToInt(string userInput, ref bool successParse)
+        {
+            int number;
+            bool result = int.TryParse(userInput, out number);
+
+            if (result == false)
+            {
+                Console.WriteLine("Не удалось сконвертировать в int. Попробуйте снова");
+            }
+            else
+            {
+                Console.WriteLine($"Успешно преобразовано в число: {number}");
+                successParse = true;
+            }
+        }
+    }
+}
