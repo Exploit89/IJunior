59ef4f007ec6a94b526d49a5bc4e46d2ba68713c
Aleksandr <74274875+Exploit89@users.noreply.github.com>
2022-05-11 21:17:55 +1000

    PC_Home commit

---
+++ MergingCollections/MergingCollections.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MergingCollections
+{
+    internal class MergingCollections
+    {
+        static void Main(string[] args)
+        {
+            string[] firstArray = { "1", "2", "5", "7" };
+            string[] secondArray = { "2", "8", "9", "5" };
+            List<string> list = new List<string>();
+
+            list.AddRange(firstArray);
+            list.AddRange(secondArray);
+            RemoveEqualValues(list);
+            list.Sort();
+
+            Console.Write($"Первый массив строк: ");
+            WriteComposition(firstArray);
+            Console.Write($"Второй массив строк: ");
+            WriteComposition(secondArray);
+            Console.Write("Итоговая коллекция: ");
+            WriteComposition(list);
+        }
+
+        static void WriteComposition(string[] array)
+        {
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        static void WriteComposition(List<string> list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.Write(list[i] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        static void RemoveEqualValues(List<string> list)
+        {
+            for(int i = 0; i < list.Count; i++)
+            {
+                for(int j = 0; j < list.Count; j++)
+                {
+                    if(list[i] == list[j] && i != j)
+                    list.Remove(list[i]);
+                }
+            }
+        }
+    }
+}
