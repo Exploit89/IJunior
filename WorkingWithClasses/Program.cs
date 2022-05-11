8df2df04cbcac9b86c7a6a1b551058494591d8af
Aleksandr <74274875+Exploit89@users.noreply.github.com>
2022-05-11 22:57:50 +1000

    PC_Home commit

---
+++ WorkingWithClasses/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace WorkingWithClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+            player.ShowStats();
+        }
+    }
+
+    class Player
+    {
+        private int _damage;
+        private int _armor;
+        private string _name;
+        private int _health;
+
+        public Player()
+        {
+            _damage = 50;
+            _armor = 20;
+            _name = "Player1";
+            _health = 100;
+        }
+
+        public void ShowStats()
+        {
+            Console.WriteLine($"Damage = {_damage}\n" +
+                $"Armor = {_armor}\n" +
+                $"Name = {_name}\n" +
+                $"Health = {_health}\n");
+        }
+    }
+}
