3ba8750867f5f55a7dc3c77670c106a1dd370904
Aleksandr <74274875+Exploit89@users.noreply.github.com>
2022-05-11 20:45:17 +1000

    PC_Home commit

---
+++ AdvancedDynamicArray/AdvancedDynamicArray.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdvancedDynamicArray
+{
+    internal class AdvancedDynamicArray
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            string userInput;
+            bool isWorking = true;
+
+            Console.WriteLine("Команда для суммирования введеных чисел - sum\nКоманда для выхода - exit");
+            Console.WriteLine("Введите число для сохранения в массив или команду: ");
+
+            while (isWorking)
+            {
+                userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case "exit":
+                        isWorking = false;
+                        break;
+                    case "sum":
+                        SumUp(numbers);
+                        break;
+                    default:
+                        AddNumber(userInput, numbers);
+                        break;
+                }
+            }
+        }
+
+        static void SumUp(List<int> numbers)
+        {
+            int sum = 0;
+
+            foreach(int number in numbers)
+            {
+                sum += number;
+            }
+            Console.WriteLine("Сумма значений массива = " + sum);
+        }
+
+        static void AddNumber(string userInput, List<int> numbers)
+        {
+            bool userNumber = int.TryParse(userInput, out int number);
+
+            if (userNumber)
+            {
+                numbers.Add(number);
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели не число. Попробуйте еще раз"); 
+            }
+        }
+    }
+}
---
+++ AdvancedPersonnelAccounting/AdvancedPersonnelAccounting.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdvancedPersonnelAccounting
+{
+    internal class AdvancedPersonnelAccounting
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> persons = new Dictionary<string, string>();
+            bool isWorking = true;
+
+            Console.WriteLine("Добро пожаловать в программу кадрового учёта!");
+
+            while (isWorking)
+            {
+                int userInput;
+
+                Console.WriteLine("\nВведите номер команды:\n" +
+                    "1 - Добавить досье\n" +
+                    "2 - Вывести все досье\n" +
+                    "3 - Удалить досье\n" +
+                    "4 - Выход\n");
+
+                userInput = Convert.ToInt32(Console.ReadLine());
+
+                switch (userInput)
+                {
+                    case 1:
+                        AddDossier(persons);
+                        break;
+                    case 2:
+                        ShowAllDossiers(persons);
+                        break;
+                    case 3:
+                        DeleteDossier(persons);
+                        break;
+                    case 4:
+                        isWorking = false;
+                        break;
+                    default:
+                        Console.WriteLine("Такой команды не существует.");
+                        break;
+                }
+            }
+        }
+
+        static void AddDossier(Dictionary<string, string> persons)
+        {
+            Console.WriteLine("Введите ФИО:");
+            string inputFullName = Console.ReadLine();
+            Console.WriteLine("Введите должность:");
+            string inputPosition = Console.ReadLine();
+            persons.Add(inputFullName, inputPosition);
+        }
+
+        static void ShowAllDossiers(Dictionary<string, string> persons)
+        {
+            Console.WriteLine("Полный список кадров:");
+            int number = 0;
+
+            foreach(var item in persons)
+            {
+                number++;
+                Console.Write($"{number}. {item.Key} - {item.Value}\n");
+            }
+        }
+
+        static void DeleteDossier(Dictionary<string, string> persons)
+        {
+            Console.WriteLine("Укажите номер досье для удаления:");
+            int dossierNumberInput = Convert.ToInt32(Console.ReadLine());
+
+            if (persons.Count == 0)
+            {
+                Console.WriteLine("Список пуст");
+            }
+            else if (dossierNumberInput < 1 || dossierNumberInput > persons.Count)
+            {
+                Console.WriteLine($"Досье под номером {dossierNumberInput} не существует.");
+            }
+            else
+            {
+                string itemKey = "";
+                int dossierCount = 0;
+
+                foreach(var item in persons)
+                {
+                    dossierCount++;
+
+                    if(dossierNumberInput == dossierCount)
+                    {
+                        itemKey = item.Key;
+                    }
+                }
+                persons.Remove(itemKey);
+            }
+        }
+    }
+}
