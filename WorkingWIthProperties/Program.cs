23e39553392c0c41ef0a890885dcf54f0fda892e
Aleksandr <74274875+Exploit89@users.noreply.github.com>
2022-05-12 20:35:22 +1000

    PC_Home commit

---
+++ WorkingWIthProperties/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace WorkingWIthProperties
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+
+            Console.WriteLine(player.PositionX);
+            Console.WriteLine(player.PositionY);
+            player.DrawPlayer();
+        }
+    }
+
+    class Player
+    {
+        public Player()
+        {
+            PositionX = 5;
+            PositionY = 3;
+        }
+
+        public int PositionX { get; private set; }
+        public int PositionY { get; private set; }
+
+        public void DrawPlayer(char playerCharacter = '@')
+        {
+            Console.SetCursorPosition(PositionX, PositionY);
+            Console.WriteLine(playerCharacter);
+        }
+    }
+}
