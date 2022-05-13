460a606e56d143784a49e869904ff52988a29f9f
Exploit89 <khudajbergenov89@gmail.com>
2022-05-13 15:08:40 +1000

    PC_Work commit

--- PlayersDatabase/Program.cs
+++ PlayersDatabase/Program.cs
@@ -1,11 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace PlayersDatabase
+namespace PlayersDB
 {
     internal class Program
     {
         static void Main(string[] args)
+        {
+            string nicknameInput = "";
+            int levelInput = 0;
+            PlayersDatabase playersDatabase = new PlayersDatabase();
+            Player player = new Player(nicknameInput, levelInput);
+
+            Console.WriteLine("Введите имя");
+            nicknameInput = Console.ReadLine();
+            Console.WriteLine("Введите уровень");
+            levelInput = Convert.ToInt32(Console.ReadLine());
+
+            player = new Player(nicknameInput, levelInput);
+            playersDatabase.AddPlayer(player);
+
+            Console.WriteLine("Введите имя");
+            nicknameInput = Console.ReadLine();
+            Console.WriteLine("Введите уровень");
+            levelInput = Convert.ToInt32(Console.ReadLine());
+
+            player = new Player(nicknameInput, levelInput);
+            playersDatabase.AddPlayer(player);
+
+            //AddPlayerToDatabase(playersDatabase);
+            //AddPlayerToDatabase(playersDatabase);
+
+            playersDatabase.ShowAllPlayers(player.GetPlayerID());
+            
+
+        }
+
+        static void AddPlayerToDatabase(PlayersDatabase playersDatabase)
         {
 
         }
@@ -15,9 +46,9 @@ namespace PlayersDatabase
     {
         private List<Player> _players;
 
-        public PlayersDatabase(List<Player> players)
+        public PlayersDatabase()
         {
-            _players = players;
+            _players = new List<Player>();
         }
 
         public void AddPlayer(Player player)
@@ -29,21 +60,31 @@ namespace PlayersDatabase
         {
             _players.Remove(player);
         }
+
+        public void ShowAllPlayers(List<string> playerID)
+        {
+            for(int i = 0; i < _players.Count; i++)
+            {
+                Console.Write($"ID: {playerID[i]} - {_players[i].Nickname} - {_players[i].Level} - {_players[i].IsBanned}");
+            }
+        }
     }
 
     class Player
     {
         private List<string> _playersID;
-        private string _nickname;
-        private int _level;
-        private bool _isBanned;
 
-        public Player(string nickname, int level, bool isBanned)
+        public string Nickname { get; private set; }
+        public int Level { get; private set; }
+        public bool IsBanned { get; private set; }
+
+        public Player(string nickname, int level)
         {
+            _playersID = new List<string>();
             _playersID.Add(CreatePlayerID(_playersID));
-            _nickname = nickname;
-            _level = level;
-            _isBanned = isBanned;
+            Nickname = nickname;
+            Level = level;
+            IsBanned = false;
         }
         
         private string CreatePlayerID(List<string> PlayersID)
@@ -72,15 +113,10 @@ namespace PlayersDatabase
             return true;
         }
 
-        public Player GetPlayer(string playerID, List<string> playersID, Player player)
+        public List<string> GetPlayerID()
         {
-            for(int i = 0; i < playersID.Count; i++)
-            {
-                if (playersID[i] == playerID)
-                {
-                    return player[i];
-                }
-            }
+            List<string> playerIDs = _playersID;
+            return playerIDs;
         }
     }
 }
