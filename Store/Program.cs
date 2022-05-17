7865f53b77abb6e07cbd666de483f5e619fe970c
Exploit89 <khudajbergenov89@gmail.com>
2022-05-17 17:42:41 +1000

    PC_Work commit

---
+++ Store/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    class Seller
+    {
+
+    }
+
+    class Product
+    {
+
+    }
+
+    class Customer
+    {
+
+    }
+
+    class Shop
+    {
+
+    }
+
+    class Cart
+    {
+
+    }
+}
