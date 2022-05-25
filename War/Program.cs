7c66523a0184820fd244dac845dbeded82be7983
Exploit89 <khudajbergenov89@gmail.com>
2022-05-25 13:01:21 +1000

    PC_Work commit

---
+++ War/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace War
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class BattleField
+    {
+        private Squad _firstSquad;
+        private Squad _secondSquad;
+    }
+
+    class Squad
+    {
+        private List<Fighter> _fighters;
+    }
+
+    class Fighter
+    {
+
+    }
+
+    class Swordsman : Fighter
+    {
+
+    }
+
+    class Archer : Fighter
+    {
+
+    }
+
+    class Ninja : Fighter
+    {
+
+    }
+
+    class Mechwarrior : Fighter
+    {
+
+    }
+}
