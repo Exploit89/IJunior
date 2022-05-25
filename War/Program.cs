65e167350ffdc0c26e50d9f86e195919e4a68865
Exploit89 <khudajbergenov89@gmail.com>
2022-05-25 17:39:57 +1000

    PC_Work commit

--- War/Program.cs
+++ War/Program.cs
@@ -17,6 +17,7 @@ namespace War
     {
         private Squad _firstSquad;
         private Squad _secondSquad;
+        private Random _random = new Random();
     }
 
     class Squad
@@ -26,26 +27,83 @@ namespace War
 
     class Fighter
     {
+        public string Name { get; private set; }
+        public string ClassName { get; protected set; }
+        public float Damage { get; protected set; }
+        public float Health { get; protected set; }
 
+        public Fighter(Random random)
+        {
+            List<string> names = new List<string>();
+            int minHealth = 100;
+            int maxHealth = 300;
+            int minDamage = 10;
+            int maxDamage = 25;
+
+            foreach (var name in Enum.GetNames(typeof(FighterName)))
+                names.Add(name);
+
+            Name = names[random.Next(names.Count)];
+            Damage = random.Next(minDamage, maxDamage);
+            Health = random.Next(minHealth, maxHealth);
+        }
     }
 
     class Swordsman : Fighter
     {
-
+        public Swordsman(Random random) : base(random)
+        {
+            ClassName = Enum.GetName(typeof(FighterClassName), FighterClassName.Swordsman);
+        }
     }
 
     class Archer : Fighter
     {
-
+        public Archer(Random random) : base(random)
+        {
+            ClassName = Enum.GetName(typeof(FighterClassName), FighterClassName.Archer);
+        }
     }
 
     class Ninja : Fighter
     {
-
+        public Ninja(Random random) : base(random)
+        {
+            ClassName = Enum.GetName(typeof(FighterClassName), FighterClassName.Ninja);
+        }
     }
 
     class Mechwarrior : Fighter
     {
+        public Mechwarrior(Random random) : base(random)
+        { 
+            ClassName = Enum.GetName(typeof(FighterClassName), FighterClassName.Mechwarrior);
+        }
+    }
 
+    enum FighterName
+    {
+        Alpha,
+        Bravo,
+        Charlie,
+        Delta,
+        Ibragim,
+        Gamma,
+        Theta,
+        Sigma,
+        Kappa,
+        Omega,
+        Upsilon,
+        Zeta,
+        Eta,
+        Kotleta
+    }
+
+    enum FighterClassName
+    {
+        Swordsman,
+        Archer,
+        Ninja,
+        Mechwarrior
     }
 }
