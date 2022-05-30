6f8c2506d3e6abda074a625a1a10639ea7ec3220
Aleksandr <74274875+Exploit89@users.noreply.github.com>
2022-05-30 11:28:34 +1000

    PC_Home commit

---
+++ DefinitionExpiration/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefinitionExpiration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Shop shop = new Shop();
+            shop.Open();
+        }
+    }
+
+    class Shop
+    {
+        private List<Product> _products;
+        private Random _random = new Random();
+        private int _currentYear = 2022;
+
+        public Shop()
+        {
+            int minProductionYear = 2019;
+            int maxProductionYear = 2022;
+            int minExpirtaionYear = 1;
+            int maxExpirtaionYear = 3;
+            _products = new List<Product>();
+            _products.Add(new Product("Тушенка", _random.Next(minProductionYear, maxProductionYear), _random.Next(minExpirtaionYear, maxExpirtaionYear)));
+            _products.Add(new Product("Грибы", _random.Next(minProductionYear, maxProductionYear), _random.Next(minExpirtaionYear, maxExpirtaionYear)));
+            _products.Add(new Product("Корнишоны", _random.Next(minProductionYear, maxProductionYear), _random.Next(minExpirtaionYear, maxExpirtaionYear)));
+            _products.Add(new Product("Помидоры", _random.Next(minProductionYear, maxProductionYear), _random.Next(minExpirtaionYear, maxExpirtaionYear)));
+            _products.Add(new Product("Огурцы", _random.Next(minProductionYear, maxProductionYear), _random.Next(minExpirtaionYear, maxExpirtaionYear)));
+            _products.Add(new Product("Перец", _random.Next(minProductionYear, maxProductionYear), _random.Next(minExpirtaionYear, maxExpirtaionYear)));
+            _products.Add(new Product("Горох", _random.Next(minProductionYear, maxProductionYear), _random.Next(minExpirtaionYear, maxExpirtaionYear)));
+            _products.Add(new Product("Кукуруза", _random.Next(minProductionYear, maxProductionYear), _random.Next(minExpirtaionYear, maxExpirtaionYear)));
+            _products.Add(new Product("Шляпаусатая", _random.Next(minProductionYear, maxProductionYear), _random.Next(minExpirtaionYear, maxExpirtaionYear)));
+            _products.Add(new Product("Тринитротолуол", _random.Next(minProductionYear, maxProductionYear), _random.Next(minExpirtaionYear, maxExpirtaionYear)));
+            _products.Add(new Product("Зубной порошок", _random.Next(minProductionYear, maxProductionYear), _random.Next(minExpirtaionYear, maxExpirtaionYear)));
+        }
+
+        public void Open()
+        {
+            bool isOpen = true;
+
+            while (isOpen)
+            {
+                Console.WriteLine(" \nМеню:" +
+                    " \n1. Показать просрочку" +
+                    " \n2. Показать все товары" +
+                    " \n3. Выход\n");
+                ConsoleKeyInfo charKey = Console.ReadKey();
+
+                switch (charKey.Key)
+                {
+                    case ConsoleKey.D1:
+                        ShowExpiredProducts();
+                        break;
+                    case ConsoleKey.D2:
+                        ShowAllProducts();
+                        break;
+                    case ConsoleKey.D3:
+                        isOpen = false;
+                        break;
+                    default:
+                        ShowDefaultMessage();
+                        break;
+                }
+            }
+        }
+
+        private void ShowExpiredProducts()
+        {
+            Console.Clear();
+            Console.WriteLine("Просроченные товары: ");
+            var sortedProducts = _products.Where(product => _currentYear - product.ProductionYear > product.ExpirationYear);
+
+            foreach (var product in sortedProducts)
+                Console.WriteLine($"{product.Name}. Год изготовления: {product.ProductionYear}. Срок годности: {product.ExpirationYear} год(лет)");
+        }
+
+        private void ShowAllProducts()
+        {
+            Console.Clear();
+            Console.WriteLine("Все товары: ");
+
+            foreach (var product in _products)
+                Console.WriteLine($"{product.Name}. Год изготовления: {product.ProductionYear}. Срок годности: {product.ExpirationYear}");
+        }
+
+        private void ShowDefaultMessage()
+        {
+            Console.Clear();
+            Console.WriteLine("Такой команды не существует.");
+        }
+    }
+
+    class Product
+    {
+        public string Name { get; private set; }
+        public int ProductionYear { get; private set; }
+        public int ExpirationYear { get; private set; }
+
+        public Product(string name, int productionYear, int expirationYear)
+        {
+            Name = name;
+            ProductionYear = productionYear;
+            ExpirationYear = expirationYear;
+        }
+    }
+}
---
+++ TopServerPlayers/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TopServerPlayers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Server server = new Server();
+            server.Open();
+        }
+    }
+
+    class Server
+    {
+        private List<Player> _players;
+        private Random _random = new Random();
+
+        public Server()
+        {
+            int maxLevel = 101;
+            int maxPower = 300;
+            _players = new List<Player>();
+            _players.Add(new Player("Balalaka", _random.Next(maxLevel), _random.Next(maxPower)));
+            _players.Add(new Player("Pes", _random.Next(maxLevel), _random.Next(maxPower)));
+            _players.Add(new Player("Drudnik", _random.Next(maxLevel), _random.Next(maxPower)));
+            _players.Add(new Player("artyRus", _random.Next(maxLevel), _random.Next(maxPower)));
+            _players.Add(new Player("God19218", _random.Next(maxLevel), _random.Next(maxPower)));
+            _players.Add(new Player("_killer2012", _random.Next(maxLevel), _random.Next(maxPower)));
+            _players.Add(new Player("invalid", _random.Next(maxLevel), _random.Next(maxPower)));
+            _players.Add(new Player("groomBox", _random.Next(maxLevel), _random.Next(maxPower)));
+            _players.Add(new Player("qwerty123", _random.Next(maxLevel), _random.Next(maxPower)));
+            _players.Add(new Player("TaokaNomakami", _random.Next(maxLevel), _random.Next(maxPower)));
+            _players.Add(new Player("kasjdfnas", _random.Next(maxLevel), _random.Next(maxPower)));
+        }
+
+        public void Open()
+        {
+            bool isOpen = true;
+
+            while (isOpen)
+            {
+                Console.WriteLine(" \nМеню:" +
+                    " \n1. Топ-3 по уровню" +
+                    " \n2. Топ-3 по силе" +
+                    " \n3. Все игроки" +
+                    " \n4. Выход\n");
+                ConsoleKeyInfo charKey = Console.ReadKey();
+
+                switch (charKey.Key)
+                {
+                    case ConsoleKey.D1:
+                        ShowTopByLevel();
+                        break;
+                    case ConsoleKey.D2:
+                        ShowTopByPower();
+                        break;
+                    case ConsoleKey.D3:
+                        ShowAllPlayers();
+                        break;
+                    case ConsoleKey.D4:
+                        isOpen = false;
+                        break;
+                    default:
+                        ShowDefaultMessage();
+                        break;
+                }
+            }
+        }
+
+        private void ShowTopByLevel()
+        {
+            Console.Clear();
+            Console.WriteLine("Топ-3 по уровню: ");
+            var sortedPlayers = _players.OrderByDescending(player => player.Level).Take(3);
+
+            foreach (var player in sortedPlayers)
+                Console.WriteLine($"{player.Name}. Уровень: {player.Level}. Сила: {player.Power}");
+        }
+
+        private void ShowTopByPower()
+        {
+            Console.Clear();
+            Console.WriteLine("Топ-3 по силе: ");
+            var sortedPlayers = _players.OrderByDescending(player => player.Power).Take(3);
+
+            foreach (var player in sortedPlayers)
+                Console.WriteLine($"{player.Name}. Уровень: {player.Level}. Сила: {player.Power}");
+        }
+
+        private void ShowAllPlayers()
+        {
+            Console.Clear();
+            Console.WriteLine("Все игроки: ");
+
+            foreach (var player in _players)
+                Console.WriteLine($"{player.Name}. Уровень: {player.Level}. Сила: {player.Power}");
+        }
+
+        private void ShowDefaultMessage()
+        {
+            Console.Clear();
+            Console.WriteLine("Такой команды не существует.");
+        }
+    }
+
+    class Player
+    {
+        public string Name { get; private set; }
+        public int Level { get; private set; }
+        public int Power { get; private set; }
+
+        public Player(string name, int level, int power)
+        {
+            Name = name;
+            Level = level;
+            Power = power;
+        }
+    }
+}
